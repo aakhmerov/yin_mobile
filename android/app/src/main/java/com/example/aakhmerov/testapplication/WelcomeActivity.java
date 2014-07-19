e23da3efec21790507cbc8cb9d05ea9460e09e8a
Askar Akhmerov <aakhmerov@ostdlabs.com>
2014-07-19 14:36:30 +0200

    1. initial basics version

---
+++ android/app/src/main/java/com/example/aakhmerov/testapplication/WelcomeActivity.java
@@ -0,0 +1,48 @@
+package com.example.aakhmerov.testapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.aakhmerov.testapplication.R;
+
+public class WelcomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.welcome, menu);
+        return true;
+    }
+
+    /**
+     * Navigate to locator itself
+     * @param view
+     */
+    public void navigateToLocator(View view) {
+        Intent intent = new Intent(this, LocatorActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
